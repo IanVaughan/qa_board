acd63a622c66aa82dad65d083499e4f4dd12b2c3
Ian Vaughan <github@ianvaughan.co.uk>
2013-01-27 00:00:41 +0000

    remove some debug

--- server.rb
+++ server.rb
@@ -16,19 +16,13 @@ class Server < Sinatra::Base
   # before { env['rack.logger'] = Logger.new("log/sinatra.log", "a+")  }
   # before { env['rack.errors'] = '~/app.error.log'  }
 
-  # set :sessions, true # is this client side?
-
   REQUIRED_KEYS = [:host, :ticket, :who]
-  get '/update' do # post type type must be json
-    content_type :json # content_type 'application/json'
+  get '/update/?' do
+    content_type :json
     params = request.env['rack.request.query_hash']
-    # logger.debug "/update #{params.inspect}"
-    # logger.debug "/update #{REQUIRED_KEYS.inspect}"
 
     # f = REQUIRED_KEYS.all? { |required| params.include?(required.to_s) }
-    # logger.debug "/update f:#{f}"
     # return unless f
-    # logger.debug "/update - updating : #{params['host']}, #{params['ticket']}, #{params['who']}"
     return {missing: 'host'}.to_json unless params.has_key? 'host'
     return {missing: 'ticket'}.to_json unless params.has_key? 'ticket'
     return {missing: 'who'}.to_json unless params.has_key? 'who'
@@ -37,17 +31,7 @@ class Server < Sinatra::Base
     @@phase.data.to_json
   end
 
-  # get '/update/:host/:ticket/:who' do |host, ticket, who| # post/put/patch?
-  #   logger.debug "/update/:host/:ticket/ #{params.inspect}"
-  #   content_type 'application/json'
-  #   # post type type must be json
-  #   logger.debug "#{host}, #{ticket}, #{who}"
-  #   Test.update host, ticket, who
-  #   Test.data.to_json
-  # end
-
   get '/?.?:format?' do
-    logger.debug "/ #{params.inspect}"
     if params[:format] == 'json'
       content_type 'application/json'
       @@phase.data.to_json

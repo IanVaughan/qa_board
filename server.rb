b1da63940128d2906f594e10fc1d1b26beb0eaa5
Ian Vaughan <github@ianvaughan.co.uk>
2013-01-27 00:09:22 +0000

    move valid check into helper

--- server.rb
+++ server.rb
@@ -17,15 +17,25 @@ class Server < Sinatra::Base
   # before { env['rack.errors'] = '~/app.error.log'  }
 
   REQUIRED_KEYS = [:host, :ticket, :who]
+  helpers do
+    def valid_request? params
+      # f = REQUIRED_KEYS.all? { |required| params.include?(required.to_s) }
+      # return unless f
+      # param_keys = params.keys.map {|k| k.downcase.to_sym }
+      # REQUIRED_KEYS.each {|k| return {missing: k}.to_json unless param_keys.has_key? k }
+      return {missing: 'host'}.to_json unless params.has_key? 'host'
+      return {missing: 'ticket'}.to_json unless params.has_key? 'ticket'
+      return {missing: 'who'}.to_json unless params.has_key? 'who'
+      true
+    end
+  end
+
   get '/update/?' do
     content_type :json
     params = request.env['rack.request.query_hash']
 
-    # f = REQUIRED_KEYS.all? { |required| params.include?(required.to_s) }
-    # return unless f
-    return {missing: 'host'}.to_json unless params.has_key? 'host'
-    return {missing: 'ticket'}.to_json unless params.has_key? 'ticket'
-    return {missing: 'who'}.to_json unless params.has_key? 'who'
+    valid = valid_request? params
+    return valid unless valid == true
 
     @@phase.update params['host'], params['ticket'], params['who']
     @@phase.data.to_json
